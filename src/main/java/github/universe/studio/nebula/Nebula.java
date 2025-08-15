adf85c9b8a160878f1c4ad14ba68cff2c2ef4683
DanielH131COL <84680174+DanielH131COL@users.noreply.github.com>
2025-08-14 21:04:00 -0500

    Necessary things were imported.

---
+++ src/main/java/github/universe/studio/nebula/Nebula.java
@@ -0,0 +1,68 @@
+package github.universe.studio.nebula;
+
+import github.universe.studio.nebula.commands.NebulaCommand;
+import github.universe.studio.nebula.commands.StreamCommand;
+import github.universe.studio.nebula.listeners.Announcer;
+import github.universe.studio.nebula.utils.CC;
+import github.universe.studio.nebula.utils.ConfigManager;
+import net.md_5.bungee.api.plugin.Plugin;
+
+/**
+ * @author DanielH131COL
+ * @created 14/08/2025
+ * @project nebula
+ * @file Nebula
+ */
+public final class Nebula extends Plugin {
+    private static Nebula instance;
+    private ConfigManager configManager;
+    private Announcer announcer;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        CC.console("&b&lNEBULA &7⇨ &fProxyCore");
+        CC.console("        &a&lENABLED");
+        CC.console(" &7⇨ &fVersion: &b" + getDescription().getVersion());
+        CC.console(" &7⇨ &fAuthor: &b" + getDescription().getAuthor());
+        CC.console("");
+        CC.console(" &bThis plugin will be free until a limited version,");
+        CC.console(" &bso take advantage.");
+        CC.console("");
+
+        configManager = new ConfigManager(this);
+        configManager.load();
+        announcer = new Announcer(this);
+        announcer.start();
+        getProxy().getPluginManager().registerCommand(this, new NebulaCommand());
+        getProxy().getPluginManager().registerCommand(this, new StreamCommand(this));
+    }
+
+    @Override
+    public void onDisable() {
+
+        CC.console("&b&lNEBULA &7⇨ &fProxyCore");
+        CC.console("        &c&lDISABLED");
+        CC.console(" &7⇨ &fVersion: &b" + getDescription().getVersion());
+        CC.console(" &7⇨ &fAuthor: &b" + getDescription().getAuthor());
+        CC.console("");
+
+        configManager.saveConfig();
+        configManager.saveMessages();
+        configManager.saveAnnouncements();
+        announcer.stop();
+    }
+
+    public static Nebula getInstance() {
+        return instance;
+    }
+
+    public static ConfigManager getConfigManager() {
+        return instance.configManager;
+    }
+
+    public Announcer getAnnouncer() {
+        return announcer;
+    }
+}
