1dc826e3f1d0e58318af0253db1304262a1406f1
DanielH131COL <84680174+DanielH131COL@users.noreply.github.com>
2025-08-28 18:27:23 -0500

    Blacklist temporal de IPs

---
+++ src/main/java/github/universe/studio/nebula/antibots/BlacklistManager.java
@@ -0,0 +1,32 @@
+package github.universe.studio.nebula.antibots;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author DanielH131COL
+ * @created 28/08/2025
+ * @project nebula
+ * @file BlacklistManager
+ */
+public class BlacklistManager {
+    private final Map<String, Long> bannedIPs = new ConcurrentHashMap<>();
+    private final long banDuration;
+
+    public BlacklistManager(long banDuration) {
+        this.banDuration = banDuration;
+    }
+
+    public void ban(String ip) {
+        bannedIPs.put(ip, System.currentTimeMillis() + banDuration);
+    }
+
+    public boolean isBanned(String ip) {
+        if (!bannedIPs.containsKey(ip)) return false;
+        if (System.currentTimeMillis() > bannedIPs.get(ip)) {
+            bannedIPs.remove(ip);
+            return false;
+        }
+        return true;
+    }
+}
