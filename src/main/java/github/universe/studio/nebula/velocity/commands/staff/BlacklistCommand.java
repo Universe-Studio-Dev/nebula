7fbb10fd6848e0f962d0d1d4c18a665177723773
DanielH131COL <84680174+DanielH131COL@users.noreply.github.com>
2025-09-04 20:42:07 -0500

    Se agrego el BlacklistCommand en el Velocity

---
+++ src/main/java/github/universe/studio/nebula/velocity/commands/staff/BlacklistCommand.java
@@ -0,0 +1,116 @@
+package github.universe.studio.nebula.velocity.commands.staff;
+
+import com.velocitypowered.api.command.CommandSource;
+import com.velocitypowered.api.command.SimpleCommand;
+import com.velocitypowered.api.proxy.Player;
+import com.velocitypowered.api.proxy.ProxyServer;
+import github.universe.studio.nebula.velocity.utils.CC;
+import github.universe.studio.nebula.velocity.utils.ConfigManager;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import org.spongepowered.configurate.ConfigurationNode;
+
+/**
+ * @author DanielH131COL
+ * @created 04/09/2025
+ * @project nebula
+ * @file BlacklistCommand
+ */
+public class BlacklistCommand implements SimpleCommand {
+
+    private final ProxyServer server;
+
+    public BlacklistCommand(ProxyServer server) {
+        this.server = server;
+    }
+
+    @Override
+    public void execute(Invocation invocation) {
+        CommandSource sender = invocation.source();
+        String[] args = invocation.arguments();
+
+        if (args.length < 2) {
+            sender.sendMessage(LegacyComponentSerializer.legacyAmpersand().deserialize(CC.translate(
+                    ConfigManager.getMessages().node("blacklist", "usage-add").getString("&cUsage: /blacklist add <player> <reason>")
+            )));
+            sender.sendMessage(LegacyComponentSerializer.legacyAmpersand().deserialize(CC.translate(
+                    ConfigManager.getMessages().node("blacklist", "usage-remove").getString("&cUsage: /blacklist remove <player>")
+            )));
+            return;
+        }
+
+        String action = args[0].toLowerCase();
+        String targetName = args[1].toLowerCase();
+        ConfigurationNode config = ConfigManager.getConfig();
+
+        if (action.equals("add")) {
+            if (args.length < 3) {
+                sender.sendMessage(LegacyComponentSerializer.legacyAmpersand().deserialize(CC.translate(
+                        ConfigManager.getMessages().node("blacklist", "no-reason").getString("&cPlease provide a reason")
+                )));
+                return;
+            }
+
+            String reason = String.join(" ", args).substring(args[0].length() + args[1].length() + 2);
+
+            try {
+                config.node("blacklist", targetName).set(String.class, reason);
+                ConfigManager.saveConfig();
+            } catch (Exception e) {
+                sender.sendMessage(LegacyComponentSerializer.legacyAmpersand().deserialize(
+                        CC.translate("&cError saving blacklist: " + e.getMessage())
+                ));
+                return;
+            }
+
+            sender.sendMessage(LegacyComponentSerializer.legacyAmpersand().deserialize(CC.translate(
+                    ConfigManager.getMessages().node("blacklist", "added").getString("&a%player% has been blacklisted: %reason%")
+                            .replace("%player%", targetName)
+                            .replace("%reason%", reason)
+            )));
+
+            server.getAllPlayers().forEach(p -> p.sendMessage(LegacyComponentSerializer.legacyAmpersand().deserialize(CC.translate(
+                    ConfigManager.getMessages().node("blacklist", "broadcast-add").getString("&c%player% has been blacklisted: %reason%")
+                            .replace("%player%", targetName)
+                            .replace("%reason%", reason)
+            ))));
+
+            server.getPlayer(targetName).ifPresent(target -> target.disconnect(LegacyComponentSerializer.legacyAmpersand().deserialize(CC.translate(
+                    ConfigManager.getMessages().node("blacklist", "kick-message").getString("&cYou are blacklisted: %reason%")
+                            .replace("%reason%", reason)
+            ))));
+
+        } else if (action.equals("remove")) {
+            if (!config.node("blacklist", targetName).empty()) {
+                try {
+                    config.node("blacklist", targetName).set(null);
+                    ConfigManager.saveConfig();
+                } catch (Exception e) {
+                    sender.sendMessage(LegacyComponentSerializer.legacyAmpersand().deserialize(
+                            CC.translate("&cError removing blacklist: " + e.getMessage())
+                    ));
+                    return;
+                }
+
+                sender.sendMessage(LegacyComponentSerializer.legacyAmpersand().deserialize(CC.translate(
+                        ConfigManager.getMessages().node("blacklist", "removed").getString("&a%player% has been removed from blacklist")
+                                .replace("%player%", targetName)
+                )));
+
+                server.getAllPlayers().forEach(p -> p.sendMessage(LegacyComponentSerializer.legacyAmpersand().deserialize(CC.translate(
+                        ConfigManager.getMessages().node("blacklist", "broadcast-remove").getString("&a%player% has been removed from blacklist")
+                                .replace("%player%", targetName)
+                ))));
+            } else {
+                sender.sendMessage(LegacyComponentSerializer.legacyAmpersand().deserialize(CC.translate(
+                        ConfigManager.getMessages().node("blacklist", "already-removed").getString("&c%player% is not blacklisted")
+                                .replace("%player%", targetName)
+                )));
+            }
+        } else {
+            sender.sendMessage(LegacyComponentSerializer.legacyAmpersand().deserialize(
+                    CC.translate("&cInvalid action. Use add/remove.")
+            ));
+        }
+    }
+}
