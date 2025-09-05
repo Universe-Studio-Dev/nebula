dc0970da451271db145bb395fe5685960bcbe90b
DanielH131COL <84680174+DanielH131COL@users.noreply.github.com>
2025-09-04 20:41:31 -0500

    Added Velocity support

---
+++ src/main/java/github/universe/studio/nebula/bungee/BungeePlugin.java
@@ -0,0 +1,129 @@
+package github.universe.studio.nebula.bungee;
+
+import github.universe.studio.nebula.Nebula;
+import github.universe.studio.nebula.bungee.BungeePlugin;
+import github.universe.studio.nebula.bungee.commands.NebulaCommand;
+import github.universe.studio.nebula.bungee.commands.StreamCommand;
+import github.universe.studio.nebula.bungee.commands.message.MsgCommand;
+import github.universe.studio.nebula.bungee.commands.message.ReplyCommand;
+import github.universe.studio.nebula.bungee.commands.player.HelpopCommand;
+import github.universe.studio.nebula.bungee.commands.player.HubCommand;
+import github.universe.studio.nebula.bungee.commands.player.PingCommand;
+import github.universe.studio.nebula.bungee.commands.player.ReportCommand;
+import github.universe.studio.nebula.bungee.commands.staff.BlacklistCommand;
+import github.universe.studio.nebula.bungee.commands.staff.InfoCommand;
+import github.universe.studio.nebula.bungee.commands.staff.MaintenanceCommand;
+import github.universe.studio.nebula.bungee.commands.staff.StaffChatCommand;
+import github.universe.studio.nebula.bungee.listeners.Announcer;
+import github.universe.studio.nebula.bungee.listeners.GeneralListeners;
+import github.universe.studio.nebula.bungee.listeners.MotdListener;
+import github.universe.studio.nebula.bungee.listeners.StaffChatListener;
+import github.universe.studio.nebula.bungee.others.*;
+import github.universe.studio.nebula.bungee.utils.CC;
+import github.universe.studio.nebula.bungee.utils.ConfigManager;
+import net.md_5.bungee.api.plugin.Plugin;
+
+/**
+ * @author DanielH131COL
+ * @created 4/09/2025
+ * @project nebula
+ * @file bungee
+ */
+
+public class BungeePlugin extends Plugin {
+
+    private static BungeePlugin instance;
+    private ConfigManager configManager;
+    private Announcer announcer;
+    private StaffChatListener staffChatListener;
+    private ConnectionLimiter connectionLimiter;
+    private GlobalFloodDetector floodDetector;
+    private BlacklistManager blacklistManager;
+    private NameValidator nameValidator;
+    private CaptchaManager captchaManager;
+
+    @Override
+    public void onEnable() {
+        Nebula.initBungee(this);
+        instance = this;
+
+        CC.console("&b&lNEBULA &7⇨ &fProxyCore");
+        CC.console("        &a&lENABLED");
+        CC.console(" &7⇨ &fVersion: &b" + getDescription().getVersion());
+        CC.console(" &7⇨ &fAuthor: &b" + getDescription().getAuthor());
+        CC.console(" &7⇨ &fDiscord: &bhttps://discord.gg/jGKm94fMAk");
+        CC.console("");
+        CC.console(" &bThis plugin will be free until a limited version,");
+        CC.console(" &bso take advantage.");
+        CC.console("");
+
+        configManager = new ConfigManager(this);
+        configManager.load();
+        announcer = new Announcer(this);
+        announcer.start();
+
+        connectionLimiter = new ConnectionLimiter(3000);
+        floodDetector = new GlobalFloodDetector(50, 5000);
+        blacklistManager = new BlacklistManager(5 * 60 * 1000);
+        nameValidator = new NameValidator();
+
+        boolean captchaEnabled = getConfigManager().getConfig().getBoolean("captcha.enabled", true);
+        String captchaServer = getConfigManager().getConfig().getString("captcha.server");
+        String lobbyServer = getConfigManager().getConfig().getString("captcha.lobby");
+        captchaManager = new CaptchaManager(captchaEnabled, captchaServer, lobbyServer);
+
+        getProxy().getPluginManager().registerListener(this, new ConnectionListener(this, connectionLimiter, floodDetector, blacklistManager, nameValidator));
+
+        staffChatListener = new StaffChatListener(this);
+        getProxy().getPluginManager().registerListener(this, staffChatListener);
+        getProxy().getPluginManager().registerListener(this, new GeneralListeners(this));
+        getProxy().getPluginManager().registerListener(this, new MotdListener(this));
+
+        getProxy().getPluginManager().registerCommand(this, new MsgCommand());
+        getProxy().getPluginManager().registerCommand(this, new ReplyCommand());
+        getProxy().getPluginManager().registerCommand(this, new BlacklistCommand());
+        getProxy().getPluginManager().registerCommand(this, new NebulaCommand());
+        getProxy().getPluginManager().registerCommand(this, new PingCommand());
+        getProxy().getPluginManager().registerCommand(this, new InfoCommand());
+        getProxy().getPluginManager().registerCommand(this, new MaintenanceCommand(this));
+        getProxy().getPluginManager().registerCommand(this, new HubCommand(this));
+        getProxy().getPluginManager().registerCommand(this, new ReportCommand(this));
+        getProxy().getPluginManager().registerCommand(this, new HelpopCommand(this));
+        getProxy().getPluginManager().registerCommand(this, new StaffChatCommand(this, staffChatListener));
+        getProxy().getPluginManager().registerCommand(this, new StreamCommand(this));
+    }
+
+    @Override
+    public void onDisable() {
+        CC.console("&b&lNEBULA &7⇨ &fProxyCore");
+        CC.console("        &c&lDISABLED");
+        CC.console(" &7⇨ &fVersion: &b" + getDescription().getVersion());
+        CC.console(" &7⇨ &fAuthor: &b" + getDescription().getAuthor());
+        CC.console("");
+
+        configManager.saveConfig();
+        configManager.saveMessages();
+        configManager.saveAnnouncements();
+        announcer.stop();
+    }
+
+    public static BungeePlugin getInstance() {
+        return instance;
+    }
+
+    public static ConfigManager getConfigManager() {
+        return instance.configManager;
+    }
+
+    public Announcer getAnnouncer() {
+        return announcer;
+    }
+
+    public StaffChatListener getStaffChatListener() {
+        return staffChatListener;
+    }
+
+    public GlobalFloodDetector getFloodDetector() {
+        return floodDetector;
+    }
+}
---
+++ src/main/java/github/universe/studio/nebula/velocity/VelocityPlugin.java
@@ -0,0 +1,96 @@
+package github.universe.studio.nebula.velocity;
+
+import com.google.inject.Inject;
+import com.velocitypowered.api.command.CommandManager;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.plugin.PluginContainer;
+import com.velocitypowered.api.plugin.annotation.DataDirectory;
+import com.velocitypowered.api.proxy.ProxyServer;
+import github.universe.studio.nebula.Nebula;
+import github.universe.studio.nebula.velocity.commands.*;
+import github.universe.studio.nebula.velocity.commands.message.MsgCommand;
+import github.universe.studio.nebula.velocity.commands.message.ReplyCommand;
+import github.universe.studio.nebula.velocity.commands.player.HelpopCommand;
+import github.universe.studio.nebula.velocity.commands.player.HubCommand;
+import github.universe.studio.nebula.velocity.commands.player.PingCommand;
+import github.universe.studio.nebula.velocity.commands.player.ReportCommand;
+import github.universe.studio.nebula.velocity.commands.staff.BlacklistCommand;
+import github.universe.studio.nebula.velocity.commands.staff.InfoCommand;
+import github.universe.studio.nebula.velocity.commands.staff.MaintenanceCommand;
+import github.universe.studio.nebula.velocity.commands.staff.StaffChatCommand;
+import github.universe.studio.nebula.velocity.listeners.Announcer;
+import github.universe.studio.nebula.velocity.listeners.GeneralListeners;
+import github.universe.studio.nebula.velocity.listeners.MotdListener;
+import github.universe.studio.nebula.velocity.listeners.StaffChatListener;
+import github.universe.studio.nebula.velocity.utils.CC;
+import org.slf4j.Logger;
+
+import java.nio.file.Path;
+
+/**
+ * @author DanielH131COL
+ * @created 04/09/2025
+ * @project nebula
+ * @file VelocityPlugin
+ */
+@Plugin(id = "nebula", name = "Nebula", version = "1.3", authors = {"Universe Studio"})
+public class VelocityPlugin {
+
+    private final ProxyServer server;
+    private final Logger logger;
+    private final PluginContainer pluginContainer;
+    private final Path dataDirectory;
+    private Announcer announcer;
+    private StaffChatListener staffChatListener;
+
+    @Inject
+    public VelocityPlugin(ProxyServer server, Logger logger, PluginContainer pluginContainer, @DataDirectory Path dataDirectory) {
+        this.server = server;
+        this.logger = logger;
+        this.pluginContainer = pluginContainer;
+        this.dataDirectory = dataDirectory;
+    }
+
+    public Path getDataDirectory() {
+        return dataDirectory;
+    }
+
+    public Logger getLogger() {
+        return logger;
+    }
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+        Nebula.initVelocity(this);
+
+        CC cc = new CC(server);
+        announcer = new Announcer(this, server, cc);
+        staffChatListener = new StaffChatListener(this, server, cc);
+        GeneralListeners generalListeners = new GeneralListeners(this, server, cc, pluginContainer);
+        MotdListener motdListener = new MotdListener(this, server, cc);
+
+        server.getEventManager().register(this, generalListeners);
+        server.getEventManager().register(this, motdListener);
+        server.getEventManager().register(this, staffChatListener);
+
+        announcer.start();
+
+        CommandManager commandManager = server.getCommandManager();
+        commandManager.register(commandManager.metaBuilder("msg").aliases("tell", "w", "mensaje").build(), new MsgCommand(server));
+        commandManager.register(commandManager.metaBuilder("reply").aliases("r", "responder").build(), new ReplyCommand(server));
+        commandManager.register(commandManager.metaBuilder("helpop").aliases("ayuda").build(), new HelpopCommand(this, server));
+        commandManager.register(commandManager.metaBuilder("hub").aliases("lobby").build(), new HubCommand(this, server));
+        commandManager.register(commandManager.metaBuilder("ping").build(), new PingCommand(server));
+        commandManager.register(commandManager.metaBuilder("report").aliases("reporte").build(), new ReportCommand(this, server));
+        commandManager.register(commandManager.metaBuilder("blacklist").build(), new BlacklistCommand(server));
+        commandManager.register(commandManager.metaBuilder("info").aliases("whois").build(), new InfoCommand(server));
+        commandManager.register(commandManager.metaBuilder("maintenance").aliases("mantenimiento").build(), new MaintenanceCommand(this, server));
+        commandManager.register(commandManager.metaBuilder("sc").aliases("staffchat").build(), new StaffChatCommand(this, staffChatListener));
+        commandManager.register(commandManager.metaBuilder("nebula").aliases("n").build(), new NebulaCommand(this, server, announcer, pluginContainer));
+        commandManager.register(commandManager.metaBuilder("stream").build(), new StreamCommand(this, server));
+
+        logger.info("Nebula plugin enabled successfully.");
+    }
+}
