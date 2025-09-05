c40f8688c364dfcdf4050a2df15203ce2b87ae05
DanielH131COL <84680174+DanielH131COL@users.noreply.github.com>
2025-09-04 20:43:40 -0500

    Added ConnectionLimiter to Velocity

---
+++ src/main/java/github/universe/studio/nebula/velocity/others/ConnectionLimiter.java
@@ -0,0 +1,32 @@
+package github.universe.studio.nebula.velocity.others;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author DanielH131COL
+ * @created 04/09/2025
+ * @project nebula
+ * @file ConnectionLimiter
+ */
+
+public class ConnectionLimiter {
+    private final Map<String, Long> lastConnection = new ConcurrentHashMap<>();
+    private final int delay;
+
+    public ConnectionLimiter(int delay) {
+        this.delay = delay;
+    }
+
+    public boolean allowConnection(String ip) {
+        long now = System.currentTimeMillis();
+        if (lastConnection.containsKey(ip)) {
+            long last = lastConnection.get(ip);
+            if (now - last < delay) {
+                return false;
+            }
+        }
+        lastConnection.put(ip, now);
+        return true;
+    }
+}
