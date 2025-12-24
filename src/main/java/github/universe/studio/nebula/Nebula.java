a125ed245da27fec40b2309f97b141790f5ec83e
DanielH131COL <84680174+DanielH131COL@users.noreply.github.com>
2025-12-23 21:26:04 -0500

    The new version and more have been updated.

--- src/main/java/github/universe/studio/nebula/Nebula.java
+++ src/main/java/github/universe/studio/nebula/Nebula.java
@@ -11,11 +11,18 @@ import github.universe.studio.nebula.velocity.utils.ConfigManager;
  */
 public class Nebula {
 
+    private static VelocityPlugin velocityInstance;
+
     public static void initBungee(Object plugin) {
     }
 
     public static void initVelocity(VelocityPlugin plugin) {
+        velocityInstance = plugin;
         ConfigManager.init(plugin);
         ConfigManager.load();
+    }
+
+    public static VelocityPlugin getVelocityInstance() {
+        return velocityInstance;
     }
 }
--- src/main/java/github/universe/studio/nebula/velocity/VelocityPlugin.java
+++ src/main/java/github/universe/studio/nebula/velocity/VelocityPlugin.java
@@ -1,15 +1,22 @@
 package github.universe.studio.nebula.velocity;
 
 import com.google.inject.Inject;
+import com.velocitypowered.api.command.Command;
 import com.velocitypowered.api.command.CommandManager;
 import com.velocitypowered.api.command.CommandMeta;
 import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.player.PlayerChatEvent;
+import com.velocitypowered.api.event.player.ServerConnectedEvent;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.plugin.PluginContainer;
 import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.ProxyServer;
-import github.universe.studio.nebula.Nebula;
+import github.universe.studio.nebula.*;
+import github.universe.studio.nebula.common.ActionsEngine;
+import github.universe.studio.nebula.common.AntiBot;
+import github.universe.studio.nebula.common.InteractiveChat;
+import github.universe.studio.nebula.common.SocialSystem;
 import github.universe.studio.nebula.velocity.commands.*;
 import github.universe.studio.nebula.velocity.commands.message.IgnoreCommand;
 import github.universe.studio.nebula.velocity.commands.message.MsgCommand;
@@ -27,11 +34,15 @@ import github.universe.studio.nebula.velocity.others.CaptchaManager;
 import github.universe.studio.nebula.velocity.others.FriendManager;
 import github.universe.studio.nebula.velocity.utils.CC;
 import github.universe.studio.nebula.velocity.utils.ConfigManager;
+import net.kyori.adventure.text.Component;
 import org.slf4j.Logger;
+import org.spongepowered.configurate.ConfigurationNode;
 
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
-@Plugin(id = "nebula", name = "Nebula", version = "1.8", authors = {"Universe Studio"})
+@Plugin(id = "nebula", name = "Nebula", version = "1.9", authors = {"Universe Studio"})
 public class VelocityPlugin {
 
     private final ProxyServer server;
@@ -43,6 +54,10 @@ public class VelocityPlugin {
     private FriendManager friendManager;
     private CaptchaManager captchaManager;
     private ConfigManager configManager;
+    private Map<String, Boolean> modules = new HashMap<>();
+    private ActionsEngine actionsEngine;
+    private SocialSystem socialSystem;
+    private AntiBot antiBot;
 
     @Inject
     public VelocityPlugin(ProxyServer server, Logger logger, PluginContainer pluginContainer, @DataDirectory Path dataDirectory) {
@@ -56,13 +71,14 @@ public class VelocityPlugin {
     public void onProxyInitialization(ProxyInitializeEvent event) {
         Nebula.initVelocity(this);
         CC cc = new CC(server);
+        loadModules();
         configManager = new ConfigManager();
         ConfigManager.init(this);
         ConfigManager.load();
 
         cc.console("&b&lNEBULA (ProxyCore)");
         cc.console("       &a&lENABLED");
-        cc.console(" (Version: &b1.8)");
+        cc.console(" (Version: &b1.9)");
         cc.console(" (Author: &bUniverse Studio)");
         cc.console(" (Discord: &bhttps://discord.gg/jGKm94fMAk)");
         cc.console("");
@@ -106,6 +122,44 @@ public class VelocityPlugin {
         registerCommand(commandManager, "friend", "friends", "amigos", "amigo", "frd");
     }
 
+    private void loadModules() {
+        ConfigurationNode config = ConfigManager.getConfig();
+        if (!config.empty()) {
+            config.node("modules").childrenMap().forEach((key, value) -> {
+                modules.put(key.toString(), value.getBoolean(false));
+            });
+        }
+
+        if (modules.getOrDefault("actions", true)) {
+            actionsEngine = new ActionsEngine(this);
+            server.getEventManager().register(this, actionsEngine);
+        }
+        if (modules.getOrDefault("social", true)) {
+            socialSystem = new SocialSystem(this);
+            server.getEventManager().register(this, socialSystem);
+        }
+        if (modules.getOrDefault("antibot", false)) {
+            antiBot = new AntiBot(this);
+            server.getEventManager().register(this, antiBot);
+        }
+    }
+
+    @Subscribe
+    public void onPlayerChat(PlayerChatEvent event) {
+        if (modules.getOrDefault("messages", true)) {
+            Component message = InteractiveChat.buildInteractiveMessage(event.getPlayer(), event.getMessage());
+            server.getAllPlayers().forEach(p -> p.sendMessage(message));
+            event.setResult(PlayerChatEvent.ChatResult.denied());
+        }
+    }
+
+    @Subscribe
+    public void onServerSwitch(ServerConnectedEvent event) {
+        if (actionsEngine != null) {
+            actionsEngine.execute("on-server-switch", event.getPlayer(), event.getServer().getServerInfo().getName());
+        }
+    }
+
     private void registerCommand(CommandManager manager, String name, String... aliases) {
         if (!isCommandEnabled(name)) return;
 
@@ -116,7 +170,7 @@ public class VelocityPlugin {
         manager.register(meta, getCommandInstance(name));
     }
 
-    private com.velocitypowered.api.command.Command getCommandInstance(String name) {
+    private Command getCommandInstance(String name) {
         return switch (name.toLowerCase()) {
             case "ignore" -> new IgnoreCommand(server);
             case "msg" -> new MsgCommand(server);
@@ -140,7 +194,23 @@ public class VelocityPlugin {
         return ConfigManager.getConfig().node("commands", name).getBoolean(true);
     }
 
-    public Path getDataDirectory() { return dataDirectory; }
-    public Logger getLogger() { return logger; }
-    public CaptchaManager getCaptchaManager() { return captchaManager; }
+    public ProxyServer getServer() {
+        return server;
+    }
+
+    public Path getDataDirectory() {
+        return dataDirectory;
+    }
+
+    public Logger getLogger() {
+        return logger;
+    }
+
+    public CaptchaManager getCaptchaManager() {
+        return captchaManager;
+    }
+
+    public SocialSystem getSocialSystem() {
+        return socialSystem;
+    }
 }
